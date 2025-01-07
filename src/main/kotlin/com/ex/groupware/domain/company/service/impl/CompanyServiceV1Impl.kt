5c478942d6ac45acb4b33c172aafd38ddb1230e9
권도현 <tobeykwon@gmail.com>
2025-01-07 23:07:51 +0900

    feat: CompanyServiceV1Impl 추가

---
+++ src/main/kotlin/com/ex/groupware/domain/company/service/impl/CompanyServiceV1Impl.kt
@@ -0,0 +1,26 @@
+package com.ex.groupware.domain.company.service.impl
+
+import com.ex.groupware.domain.company.doamin.entity.Company
+import com.ex.groupware.domain.company.doamin.repository.CompanyRepository
+import com.ex.groupware.domain.company.service.CompanyServiceV1
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class CompanyServiceV1Impl(
+    val companyRepository: CompanyRepository,
+) : CompanyServiceV1 {
+
+    override fun findDetail(companyCode: String): Company {
+        return companyRepository.findById(companyCode)
+            .orElseThrow { throw RuntimeException("회사 코드가 존재하지 않습니다.")}
+    }
+
+    @Transactional
+    override fun update(companyCode: String, updateParam: Company) {
+        val company = companyRepository.findById(companyCode)
+            .orElseThrow { throw RuntimeException("회사 코드가 존재하지 않습니다.") }
+        company.update(updateParam)
+    }
+}
