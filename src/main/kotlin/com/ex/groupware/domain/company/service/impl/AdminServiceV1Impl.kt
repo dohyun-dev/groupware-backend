0269a965e5a7647ad6304b5323e50d2755270057
권도현 <tobeykwon@gmail.com>
2025-01-07 23:07:40 +0900

    feat: AdminServiceV1Impl 추가

---
+++ src/main/kotlin/com/ex/groupware/domain/company/service/impl/AdminServiceV1Impl.kt
@@ -0,0 +1,19 @@
+package com.ex.groupware.domain.company.service.impl
+
+import com.ex.groupware.domain.company.doamin.entity.Company
+import com.ex.groupware.domain.company.doamin.repository.CompanyRepository
+import com.ex.groupware.domain.company.service.AdminServiceV1
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class AdminServiceV1Impl(
+    val companyRepository: CompanyRepository,
+): AdminServiceV1 {
+
+    override fun registerCompany(companyName: String) {
+        val newCompany = Company(companyName)
+        companyRepository.saveAndFlush(newCompany)
+    }
+}
